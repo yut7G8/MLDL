15b9f5cabbcf930aee7cad2adb7540ad867e5b5e
yut7G8 <billion7g8.316@keio.jp>
2021-09-14 23:02:47 +0900

    Add ch3

---
+++ src/activationfunc.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+class Activation():
+
+    def __init__(self) -> None:
+        pass
+
+    def step_func(self, x):
+        # if x > 0:
+        #     return 1
+        # else:
+        #     return 0
+        y = x > 0
+        return y.astype(np.int)
+    
+    def plot_step(self, x):
+        y = self.step_func(x)
+        plt.plt(x, y)
+        plt.ylim(-0.1, 1.1)
+        plt.show()
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def plot_sigmoid(self, x):
+        y = self.sigmoid(x)
+        plt.plt(x, y)
+        plt.ylim(-0.1, 1.1)
+        plt.show()
+    
+    def relu(self, x):
+        return np.matrix(0, x)
+        
