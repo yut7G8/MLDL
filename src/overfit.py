9e5c1aded5bd46fc6971fd6f59d03871f03db955
yut7G8 <billion7g8.316@keio.jp>
2021-09-20 14:29:40 +0900

    Add Dropout

---
+++ src/overfit.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        
+    def backward(self, dout):
+        return dout * self.mask
